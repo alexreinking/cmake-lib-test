347902fb428779c514d5d641e068399bfcf1f599
Alex Reinking <alex.reinking@gmail.com>
2025-02-02 11:59:56 -0500

    Inline vcpkg.cmake

--- cmake/vcpkg.cmake
+++
@@ -1,7 +0,0 @@
-FetchContent_Declare(vcpkg
-    GIT_REPOSITORY https://github.com/microsoft/vcpkg/
-    GIT_TAG 2025.01.13
-)
-FetchContent_MakeAvailable(vcpkg)
-
-set(CMAKE_TOOLCHAIN_FILE "${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake" CACHE FILEPATH "")
